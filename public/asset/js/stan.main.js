9a01f36a1796d2c8f1fc65a69fcccaa36ede26f0
HP\casflo <castillofloresstevenantony@gmail.com>
2018-11-17 21:51:40 -0500

    avances

---
+++ public/asset/js/stan.main.js
@@ -0,0 +1,16 @@
+/**
+ * @Autor - Castillo Flores Steven Antony
+ * @Año - 2018
+ * @Ubicacion - Peru.Lambayeque
+ */
+
+$(document).ready(function () {
+
+  var user = $('.icon-user');
+
+  function init() {
+    user.css({animation :'initUser 0.5s linear'});
+  }
+
+  init();
+});
